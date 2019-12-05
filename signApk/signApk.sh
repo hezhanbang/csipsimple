01d6ec1337e287c19e9775b15970c0e837605f20
liyouLenovo <liyoubdu@qq.com>
2019-12-05 15:31:29 +0800

    2

--- signApk/signApk.sh
+++ signApk/signApk.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+ls -lh ../trunk/CSipSimple/bin/CSipSimple-release-unsigned.apk
+
 rm -rf CSipSimple.apk 
 java -jar signed/signapk.jar signed/platform.x509.pem signed/platform.pk8 ../trunk/CSipSimple/bin/CSipSimple-release-unsigned.apk CSipSimple.apk
